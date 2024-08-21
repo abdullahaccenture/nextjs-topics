5edc7b1399d9bd1cea38999e53bd75d0d4ecc870
Muhammad_abdullah <mabdullah.se@gmail.com>
2024-08-21 11:36:29 +0800

    async metadata

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,3 +1,13 @@
+export const generateMetadata = ({
+  params,
+}: {
+  params: { postId: string };
+}) => {
+  console.log(params.postId);
+  return {
+    title: "this is post from  postid  " + params.postId,
+  };
+};
 export default function PostDetail() {
   return <div>Post detail</div>;
 }
--- app/posts/[postId]/reviews/[reviewId]/page.tsx
+++ app/posts/[postId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,21 @@
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import React from "react";
+
+type Props = {
+  params: {
+    productid: string;
+    reviewId: string;
+  };
+};
+
+export const generateMetadata = async (params: Props): Promise<Metadata> => {
+  const aynctitle = await Promise.resolve(" hi this is await");
+  console.log(aynctitle);
+  return {
+    title: "Review page title" + aynctitle,
+  };
+};
 
 export default function ReviewDetail({
   params,
